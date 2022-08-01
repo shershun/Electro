b1c59ee542918be6f3dcdc8bedcb66e95cbdd53e
shershun <80419420+shershun@users.noreply.github.com>
2022-08-01 19:58:03 +0300

    Modul_1
    Розгалуження

---
+++ js/Modul_1.js
@@ -0,0 +1,68 @@
+// for (let i = 0; i < 100; i += 2) {
+//    console.log(i);
+
+// }
+// console.log('конец');
+
+
+// const minSalery = 500;
+// const maxSalery = 5000;
+// const employees = 5;
+// let totalSalery = 0;
+
+// for (let i = 0; i < employees; i += 1) {
+//    const salery = Math.round(
+//       Math.random() * (maxSalery - minSalery) + minSalery,
+//    );
+
+//    console.log(`ЗП роботника ${i} - ${salery}`);
+//    totalSalery += salery
+
+
+// }
+// console.log(totalSalery);
+
+// const min = 0;
+// const max = 10;
+// let total = 5;
+// for (let i = min; i <= max; i += 1) {
+//    if (i % 2 !== 0) {
+//      // console.log('Не чётное', i);
+//       continue;
+//    }
+
+//    console.log('чотные', i);
+//    total += i;
+
+// }
+// console.log( total);
+
+
+// let a = 4;
+// console.log(a);
+// console.log('ffg');
+
+
+
+// let rezalt = prompt("Уведомление");
+// rezalt = Number(rezalt);
+// console.log("typeof", typeof rezalt, rezalt);
+
+//let sum = (0.1*10 + 0.6*10) / 10;
+
+//sum = Math.ceil(sum)
+//console.log(sum);
+// let numberflor = 3.99;
+// numberflor = Math.floor(numberflor);
+// console.log(numberflor);
+
+// let random = Math.random() * (588 - 255) + 255;
+// random = Math.floor(random);
+// console.log(random);
+
+
+
+
+
+
+
